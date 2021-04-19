b16c6d367c7c6b0ff7338c3f7a648fd70201c2ee
Giovix92 <ggualtierone@gmail.com>
2021-04-19 12:23:38 +0200

    roomservice: rebrand to SHRP

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -44,11 +44,11 @@ except ImportError:
 DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
-custom_local_manifest = ".repo/local_manifests/potato_manifest.xml"
-custom_default_revision = "aligot-release"
-custom_dependencies = "potato.dependencies"
+custom_local_manifest = ".repo/local_manifests/shrp_manifest.xml"
+custom_default_revision = "android-9.0"
+custom_dependencies = "omni.dependencies"
 org_manifest = "devices"  # leave empty if org is provided in manifest
-org_display = "PotatoDevices"  # needed for displaying
+org_display = "SHRP-Devices"  # needed for displaying
 
 github_auth = None
 
